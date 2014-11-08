3366faa547451aab428162e9d53f9c3380911bfb
Pat S. Sabpisal <pat@uniduck.co>
2014-11-08 17:31:59 -0500

    better test

--- yaml_test.sh
+++ yaml_test.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #
 TINY='tiny'
-dirs=$(ls output)
+tc="output"
+if [ "$1" == "-c" ] 
+then
+  tc="ssabpisa/input"
+  cd ssabpisa && make clean && make && cd ..
+fi
+dirs=$(ls $tc)
 for n in ${dirs}
 do
   FAIL=0
   f=$(echo $n | cut -d'.' -f1)
   echo -n "$(tput setaf 0) Testing $(tput bold) $f..$(tput sgr0)"
+  if [ "$1" == "-c" ] 
+  then
+    cd ssabpisa && java -cp lib/antlr.jar:classes/ Micro input/$f.micro > ../output/$f.T && cd ..
+  fi
   test=$(./tiny output/$f.T | sed '/STATISTICS/q' )
   gold=$(./tiny ssabpisa/milind/$f.out | sed '/STATISTICS/q' )
   df=$(diff -b -B <(echo $test) <(echo $gold))  
