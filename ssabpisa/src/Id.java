7619e3ed0f12fe858c4ff4d681014518c403295b
Pat Sabpisal <ssabpisa@me.com>
2014-12-07 04:22:04 -0500

    Index by IR Name instead of true name (because they are useless)

--- ssabpisa/src/Id.java
+++ ssabpisa/src/Id.java
@@ -61,11 +61,11 @@ public class Id {
    public String getTiny(){
 	   if(parameter_code != null){
 		   //parameter
-		   return "$" + TinyActivationRecord.getParameter(name); 
+		   return "$" + TinyActivationRecord.getParameter("$P" + parameter_code.toString()); 
 	   }
 	   if(non_global_code != null){
 		   //local variable
-		   return "$" + TinyActivationRecord.getLocalVariable(name);
+		   return "$" + TinyActivationRecord.getLocalVariable("$L" + non_global_code.toString());
 	   }
 	   return name;
    }
